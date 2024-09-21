0d4ab14e079619dbd930caaaf394f0d275a6d459
yyeonjju <llilyha123@gmail.com>
2024-09-21 23:49:08 +0900

    Initial Commit

---
+++ KoCo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KoCo
+//
+//  Created by 하연주 on 9/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KoCo/KoCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoCoApp.swift
+//  KoCo
+//
+//  Created by 하연주 on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct KoCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
